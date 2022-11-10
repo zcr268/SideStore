9e6147c860e45facc3425b2a02e21c321f97a1d9
Joseph Mattello <mail@joemattiello.com>
2022-11-09 19:55:07 -0500

    Add Package.swift for Danger
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- Package.swift
+++ Package.swift
@@ -100,3 +100,40 @@ let package = Package(
 //         ]
 //     ]).write()
 // #endif
+
+// let package = Package(
+//   name: "SideStore",
+//   platforms: [
+//     .iOS(.v13),
+//     .tvOS(.v13),
+//   ],
+//   products: [
+//     .library(name: "DangerDeps[SideStore]", type: .dynamic, targets: ["DangerDependencies"])  // dev
+//   ],
+//   dependencies: [
+//     .package(url: "https://github.com/danger/swift.git", from: "3.0.0")  // dev
+//     // Danger Plugins
+//     // .package(url: "https://github.com/username/DangerPlugin.git", from: "0.1.0") // dev
+//   ],
+//   targets: [
+//     .target(name: "DangerDependencies", dependencies: ["Danger", "DangerPlugin"])  // dev
+//     // 	name: "SideStore",
+//     // 	// dependencies: [
+//     // 	// 	.product(name: "RxSwift", package: "RxSwift")
+//     // 	// ],
+//     // 	path: "AltStore",
+//     // 	exclude: ["Info.plist", "*.m"],
+//     // 	cSettings: cSettings,
+//     // 	cxxSettings: cxxSettings),
+
+//     // .target(
+//     // 	name: "PVLibrary-ObjC",
+//     // 	// dependencies: [
+//     // 	// 	.product(name: "RxSwift", package: "RxSwift")
+//     // 	// ],
+//     // 	path: "PVLibrary",
+//     // 	exclude: ["Info.plist", "*.swift"],
+//     // 	cSettings: cSettings,
+//     // 	cxxSettings: cxxSettings),
+//   ]
+// )
