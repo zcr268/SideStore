93b1c4d83493e9fefab825ed9705d867b1de206f
Joseph Mattello <mail@joemattiello.com>
2022-11-12 23:21:02 -0500

    WireGuard add extra source files
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

---
+++ WireguardNetworkExtension/WireguardNetworkExtension-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  WireguardNetworkExtension-Bridging-Header.h.h
+//  AltStore
+//
+//  Created by Joseph Mattiello on 11/12/22.
+//  Copyright © 2022 Riley Testut. All rights reserved.
+//
+
+#include <WireGuardKitC/WireGuardKitC.h>
+#include <WireGuardKitGo/wireguard.h>
+//#include "ringlogger.h"
